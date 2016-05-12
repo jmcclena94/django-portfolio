dbec6c2b34c6f410ac6486e9dcef5a478e114bb8
Joseph McClenahan <jmcclena94@gmail.com>
2016-05-11 23:09:41 -0700

    added main views.py

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from .views import HomepageView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^home/$', HomepageView.as_view()),
     url(r'^home/', include('projects.urls')),
 ]
