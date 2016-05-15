bf0eef4d0849b53042dc5d0eedb0fe6e032aaeb4
Joseph McClenahan <jmcclena94@gmail.com>
2016-05-15 13:54:03 -0700

    added about view

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from portfolio.views import HomepageView
+from portfolio.views import HomepageView, AboutView
 from portfolio import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^home/$', HomepageView.as_view()),
+    url(r'^about/$', AboutView.as_view()),
     url(r'^', include('projects.urls')),
 ]
 
--- portfolio/portfolio/views.py
+++ portfolio/portfolio/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomepageView(TemplateView):
     template_name = 'home-content.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
