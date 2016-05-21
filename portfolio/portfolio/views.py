b2ab4bc38fc6ab44f72ed00aea4897831b60c96b
Joseph McClenahan <jmcclena94@gmail.com>
2016-05-21 10:01:13 -0700

    added about me model

--- portfolio/portfolio/views.py
+++ portfolio/portfolio/views.py
@@ -5,5 +5,5 @@ class HomepageView(TemplateView):
     template_name = 'landing.html'
 
 
-class AboutView(TemplateView):
-    template_name = 'about.html'
+# class AboutView(TemplateView):
+#     template_name = 'about.html'
