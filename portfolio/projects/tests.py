0e0665b51c8f8f90e6147254cf203710109aa8db
Joseph McClenahan <jmcclena94@gmail.com>
2016-06-02 14:16:17 -0700

    added test test

--- portfolio/projects/tests.py
+++ portfolio/projects/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from projects.models import Project
+import factory
 
-# Create your tests here.
+
+class ProjectFactory(factory.django.DjangoModelFactory):
+    """Set up Project Factory."""
+    class Meta:
+        model = Project
+    cover = factory.django.ImageField(filename='/tmp/cover.jpg')
+    splash = factory.django.ImageField(filename='/tmp/splash.jpg')
+    title = 'title'
+    description = 'description'
+    date_uploaded = '2016-05-01'
+    url = 'www.example.com'
+    github = 'www.github.com'
+
+
+class ProjectTest(TestCase):
+    """Test project model."""
+
+    def setUp(self):
+        self.project = ProjectFactory.create()
+
+    def test_project_title(self):
+        """Test project title field."""
+        self.project.title = 'title'
