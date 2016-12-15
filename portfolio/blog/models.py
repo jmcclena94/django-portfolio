810ba877cdb03dfaf53d69d8ed0c3fabfd7c5c17
Joseph McClenahan <jmcclena94@gmail.com>
2016-12-15 09:48:55 -0800

    added blog model views

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -4,6 +4,18 @@ from django.db import models
 class BlogImage(models.Model):
     """Blog image."""
 
+    image = models.ImageField(upload_to='/blog/images/%Y-%m-%d')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+class BlogVideo(models.Model):
+    """Blog video."""
+
+    url = models.URLField(max_length=255)
+    description = models.TextField()
+
+
 class Blog(models.Model):
     """Blog model."""
 
@@ -14,3 +26,4 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     date_uploaded = models.DateField(auto_now_add=True)
+    images = models.ManyToManyField(BlogImage)
--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from blog.models import Blog
 
-# Create your views here.
+
+class BlogView(ListView):
+    """View for list of blogs."""
+
+    template_name = 'blogs.html'
+    model = Blog
+
+
+class BlogDetailView(DetailView):
+    """View for project detail."""
+
+    template_name = 'blog-detail.html'
+    model = Blog
