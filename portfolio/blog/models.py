c8b831b06b56dda5a4ee079d0c33ff7e283ff610
Joseph McClenahan <jmcclena94@gmail.com>
2016-12-15 09:11:26 -0800

    updated blog models

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogImage(models.Model):
+    """Blog image."""
+
+class Blog(models.Model):
+    """Blog model."""
+
+    class Meta:
+        ordering = ['date_uploaded']
+
+    cover = models.ImageField(upload_to='blog/covers/')
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    date_uploaded = models.DateField(auto_now_add=True)
