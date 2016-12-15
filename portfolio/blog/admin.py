4075634e3d6e5c9c5b66fd3be3347087e40d1193
Joseph McClenahan <jmcclena94@gmail.com>
2016-12-15 09:56:29 -0800

    added blog html pages, migrated

--- portfolio/blog/admin.py
+++ portfolio/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Blog, BlogVideo, BlogImage
 
-# Register your models here.
+
+admin.site.register(Blog)
+admin.site.register(BlogVideo)
+admin.site.register(BlogImage)
