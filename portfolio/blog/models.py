4ce209b02cc8845a4209f211203b1f3fe1f4a600
Joseph McClenahan <jmcclena94@gmail.com>
2016-12-17 18:10:38 -0800

    updated blog models

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 
-class BlogImage(models.Model):
-    """Blog image."""
-
-    image = models.ImageField(upload_to='blog/images/%Y-%m-%d')
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-
-
-class BlogVideo(models.Model):
-    """Blog video."""
-
-    url = models.URLField(max_length=255)
-    description = models.TextField()
+# class BlogImage(models.Model):
+#     """Blog image."""
+#
+#     image = models.ImageField(upload_to='blog/images/%Y-%m-%d')
+#     title = models.CharField(max_length=255)
+#     description = models.TextField()
+#
+#
+# class BlogVideo(models.Model):
+#     """Blog video."""
+#
+#     url = models.URLField(max_length=255)
+#     description = models.TextField()
 
 
 class Blog(models.Model):
@@ -26,5 +26,5 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     date_uploaded = models.DateField(auto_now_add=True)
-    images = models.ManyToManyField(BlogImage)
-    videos = models.ManyToManyField(BlogVideo)
+    # images = models.ManyToManyField(BlogImage)
+    # videos = models.ManyToManyField(BlogVideo)
