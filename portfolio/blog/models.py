20bae12dd753dadcff613233dfa3a1f3a19a6578
Joseph McClenahan <jmcclena94@gmail.com>
2016-12-17 18:32:14 -0800

    removed blog images and videos

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,30 +1,13 @@
 from django.db import models
 
 
-# class BlogImage(models.Model):
-#     """Blog image."""
-#
-#     image = models.ImageField(upload_to='blog/images/%Y-%m-%d')
-#     title = models.CharField(max_length=255)
-#     description = models.TextField()
-#
-#
-# class BlogVideo(models.Model):
-#     """Blog video."""
-#
-#     url = models.URLField(max_length=255)
-#     description = models.TextField()
-
-
 class Blog(models.Model):
     """Blog model."""
 
     class Meta:
-        ordering = ['date_uploaded']
+        ordering = ['-date_uploaded']
 
     cover = models.ImageField(upload_to='blog/covers/')
     title = models.CharField(max_length=255)
     text = models.TextField()
     date_uploaded = models.DateField(auto_now_add=True)
-    # images = models.ManyToManyField(BlogImage)
-    # videos = models.ManyToManyField(BlogVideo)
